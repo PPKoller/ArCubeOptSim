b72b2073d5ec937d92a28ae13fd51931dce3e30f
ppkoller <patrick_koller@gmx.ch>
2020-09-04 14:09:02 +0000

    Patrick: preparing for container run.

--- runTest.sh
+++ runTest.sh
@@ -1,1 +1,1 @@
-./build/ArgonCubeOptPh -g resources/gdml/OptSim.gdml -p resources/macros/preinit.mac -m resources/macros/test.mac -o "test.root" -G
+./build/ArgonCubeOptPh -g resources/gdml/OptSim.gdml -p resources/macros/preinit.mac -m resources/macros/test.mac -o "test.root"
