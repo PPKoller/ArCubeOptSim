f7e080e217f365f818f6f5e6d720423928ee68c8
PPKoller <patrick_koller@gmx.ch>
2020-09-17 13:23:22 +0200

    Patrick: adding hit-time distribution histograms to LUT.

--- run/run_03_OptSim_createLUT.C
+++ run/run_03_OptSim_createLUT.C
@@ -71,45 +71,60 @@ void run_03_OptSim_createLUT(){
   int Voxel_temp = -1;
   int OpChannel = -1;
   float Visibility = -1.;
-  float Time = -1.;
+  float T4 = -1.;
+  TH1F * Time = NULL;
+  //float Time = -1.;
 
   //create branches
   out_tree->Branch("Voxel", &Voxel);
   out_tree->Branch("OpChannel", &OpChannel);
   out_tree->Branch("Visibility", &Visibility);
-  out_tree->Branch("Time", &Time);
+  out_tree->Branch("T4",&T4);
+  out_tree->Branch("Time", "TH1F", &Time);
   
-  int voXYZ[3] = {0, 0, 0};
+  //variable declaration
   int voxelID = -1;
   int channelID = -1;
   int sipmID = -1;
   int hitVolID = 0;
   int hitVolIdx = -1;
-  int n = 0;
   int nChannel = 48;
   int nVox = n_vox[2]*n_vox[0]*n_vox[1];
   int hits[nChannel];
+  vector<TH1F*> timeVec;
 
+  //vector initialization
   for(i=0; i<nChannel; i++){
     hits[i] = 0;
+    timeVec.push_back(new TH1F(Form("Voxel%06d_OpChannel%02d",voxelID+1,i),"Hit time distribution",100,0.,100.));
   }
 
   //write branches
-  //loop over events
-  for(i=0; i<n_entries; i++){
+  //loop over events (<= to make sure last voxel will be filled!)
+  for(i=0; i<=n_entries; i++){
     in_tree->GetEntry(i);
 
-    if(!(i%n_evt)) voxelID += 1;
+    //new voxel / last entry
+    if(!(i%n_evt)){
+      voxelID += 1;
 
-    //fill tree and reset values
-    if(Voxel!=voxelID || i==(n_entries-1)){
+      //accept voxelID in case this is the first voxel
       if(voxelID==0){
         Voxel = voxelID;
         std::cout << "processing voxel no. " << Voxel << " of " << nVox << " ..." << std::endl;
-      }else{
+      }
+      
+      //fill tree and reset values in case this is NOT the first voxel
+      else{
+        
+        //loop over optical channels
         for(j=0; j<nChannel; j++){
           OpChannel = j;
-          Visibility = (float)hits[j]/(float)(n*n_ph);
+          Visibility = (float)hits[j]/(float)(n_evt*n_ph);
+          Time = timeVec[j];
+          T4 = Time->GetBinCenter(Time->FindFirstBinAbove(4));
+
+          //only fill if vixibility > 0
           if(Visibility>0){
             out_tree->Fill();
 
@@ -118,19 +133,34 @@ void run_03_OptSim_createLUT(){
             Voxel += 2*nVox;
             Voxel -= (2*(Voxel_temp/(n_vox[0]*n_vox[1]))+1)*(n_vox[0]*n_vox[1]);
             OpChannel = (OpChannel+nChannel/2)%nChannel;
+            timeVec[j]->SetName(Form("Voxel%06d_OpChannel%02d",Voxel,OpChannel));
             out_tree->Fill();
             Voxel = Voxel_temp;
-          }
+          }/*else{
+            std::cout << "Voxel: " << Voxel << ", OpChannel: " << j << " is has 0 visibility..." << std::endl;
+          }*/
+
+          //reset hits
           hits[j] = 0;
+
         }
 
-        //reset values
+        if(i==n_entries) break;
+
+        //accept new voxelID
         Voxel = voxelID;
         if(!(Voxel%10)) std::cout << "processing voxel no. " << Voxel << " of " << nVox << " ..." << std::endl;
-        n = 0;
+
+        //reset histos
+        timeVec.clear();
+        for(j=0; j<nChannel; j++){
+          timeVec.push_back(new TH1F(Form("Voxel%06d_OpChannel%02d",Voxel,j),"Hit time distribution",100,0.,100.));
+        }
       }
     }
-  
+
+    if(i==n_entries) break;
+
     //get optical channel ID
     for(j=0; j<totalhits; j++){
       hitVolID = hit_vol_id->at(j);
@@ -256,11 +286,9 @@ void run_03_OptSim_createLUT(){
           break;//case 17
       
       }//switch(hit_vol_index)
-      
+      timeVec[channelID]->Fill(hit_time->at(j));
       hits[channelID] += 1;
     }
-
-    n += 1;
   }
 
   //create vectors
