4213cf1339235ba930b8fed6e8d394971add11cd
PPKoller <patrick_koller@gmx.ch>
2020-09-20 23:06:24 +0200

    Patrick: changed some interface finishes.

--- runTestGUI.sh
+++ runTestGUI.sh
@@ -1,2 +1,2 @@
 #./build/ArgonCubeOptPh -g resources/gdml/OptSim.gdml -p resources/macros/preinit.mac -m resources/macros/test_gui.mac -o "test_gui.root" -G
-./build/ArgonCubeOptPh -g resources/gdml/OptSim.gdml -p resources/macros/preinit.mac -m resources/macros/OptSim_00000000.mac -o "test_gui.root"
+./build/ArgonCubeOptPh -g resources/gdml/ArCLight.gdml -p resources/macros/preinit.mac -m resources/macros/OptSim_00000000.mac -o "test_gui.root" -G
